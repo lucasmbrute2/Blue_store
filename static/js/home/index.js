cd8d43b561ab729fd6e3712742f23be4347177a2
luis-botelho <luisfellypebotelho@gmail.com>
2021-07-31 10:19:03 -0300

    Separando card de login do index (melhor comunicação com backend)

--- static/js/home/index.js
+++ static/js/home/index.js
@@ -1,17 +1,1 @@
-let loginCard = document.querySelector('#loginCard')
-let descriptionCard = document.querySelector('#descriptionCard')
-let img = document.querySelector('#sideImg')
-let loginChanger = document.querySelector('.firstBtn')
-let descriptionChanger = document.querySelector('.jsBtn')
-
-loginChanger.addEventListener('click', () =>{
-    loginCard.style.display = 'Block'
-    descriptionCard.style.display = 'none'
-    img.src = '../static/img/home/admin.svg'
-})
-descriptionChanger.addEventListener('click', () =>{
-    loginCard.style.display = 'none'
-    descriptionCard.style.display = 'Block'
-    img.src = '../static/img/home/initial.svg'
-})
-// banana 
+// 
