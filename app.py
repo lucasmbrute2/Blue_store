c610bcba7f0de10f3173de0b92d21058742ebf41
Lucas <lucasmbrute614@gmail.com>
2021-07-31 08:56:51 -0300

    Acrescentando rotas

--- app.py
+++ app.py
@@ -1,5 +1,47 @@
 from flask import Flask, render_template, redirect, request
 
-
 app = Flask(__name__)
-# chupingole
+
+acesso_usuario = 'admin'
+acesso_senha = "admin"
+
+class Login:
+    
+    def __init__(self, usuario, senha):
+        self.usuario = usuario
+        self.senha = senha
+        
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/admin', methods = ['GET','POST'])
+def admin():
+    if request.method == 'POST':
+        login = Login(
+            request.form['username'],
+            request.form['password']
+        )
+
+    if login.usuario and login.senha == acesso_usuario and acesso_senha:
+        return render_template('admin.html')
+    else: 
+        return redirect('/') #Fazer interação de acesso negado com JS
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
