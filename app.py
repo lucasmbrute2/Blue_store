ed5aac99a843e13f4c4080199befeb7c2c80a968
luis-botelho <luisfellypebotelho@gmail.com>
2021-08-06 23:26:03 -0300

    Estilizando um pouco e mexendo no flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session,flash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,10 +9,10 @@ db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ilzpkagm:tFfCmwORirxRZeVpvqJJ1vFd7YVWXOE6@kesavan.db.elephantsql.com/ilzpkagm'
 acesso_usuario = 'admin'
 acesso_senha = 'admin'
+app.secret_key = 'store'
 
 #Classe que recebe o usuário e senha dos donos do Site.
 class Login():
-    
     def __init__(self, usuario, senha):
         self.usuario = usuario
         self.senha = senha
@@ -22,41 +22,65 @@ class Vendedor(db.Model):
     nome = db.Column(db.String(50), nullable= False)
     descricao = db.Column(db.String(500), nullable =False)
     imagem = db.Column(db.String(7000), nullable=False)
-    preco = db.Column(db.Float(40), nullable = False )
+    preco = db.Column(db.Integer, nullable = False )
     def __init__(self, nome,descricao,imagem,preco):
         self.nome = nome
         self.descricao = descricao
         self.imagem = imagem
         self.preco = preco
-# Página de Login do ADM
+# Página princial
+# e principais rotas
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/loja')
+def pagina_loja():
+    return render_template('loja.html')
+@app.route('/about')
+def pagina_sobre():
+    return render_template('sobre.html')
 @app.route('/login')    
 def login():
+    session['usuario_logado'] = None
     return render_template('login.html')
+# aqui acabam as rotas principais
 
-@app.route('/voltar')
-def volta_pagina():
-    return render_template('index.html')
+# 
 
-# Validação de user e password para entrada na área do ADM
+# Rotas de altentição
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+            login = Login(
+                request.form['username'],
+                request.form['password']
+            )
+    if login.usuario and login.senha == acesso_usuario and acesso_senha:
+        session['usuario_logado'] = 'admin'
+        flash('login efetuado')
+        return redirect('/admin')
+    else:
+        flash('Erro no login, tente novamente!')
+        return redirect('/login') 
+
 @app.route('/admin', methods = ['GET','POST'])
 def admin():
-    if request.method == 'POST':
-        login = Login(
-            request.form['username'],
-            request.form['password']
-        )
-    
-    if login.usuario and login.senha == acesso_usuario and acesso_senha:
-        tabelas = Vendedor.query.all()
-        return render_template('admin.html', tabelas=tabelas, tabela='') 
-    else: 
-        return render_template('login.html')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Faça o login antes de entrar nessa rota!')
+        return redirect('/login') 
+    tabelas = Vendedor.query.all()
+    return render_template('admin.html', tabelas=tabelas, tabela='') 
+  
+@app.route('/logout')
+def volta_pagina():
+    session['usuario_logado'] = None
+    return render_template('login.html')
+# aqui terminam as toras de altentição
+
+
+# aqui começam as rotas de manipulação do banco de dados
 # CRUD- Fazendo o CREATE
-@app.route('/formulario', methods = ['GET', 'POST'])
+@app.route('/new', methods = ['GET', 'POST'])
 def new_form():
     if request.method == 'POST':
         produto = Vendedor(
@@ -68,21 +92,9 @@ def new_form():
     db.session.add(produto)
     db.session.commit()
     tabelas = Vendedor.query.all()
-    return render_template('/admin', tabelas=tabelas, tabela='')
+    return render_template('/admin.html', tabelas=tabelas, tabela='')
 
-@app.route('/add', methods = ['POST', 'GET'])
-def add_item():
-    return render_template('add.html')
-
-@app.route('/loja')
-def pagina_loja():
-    return render_template('loja.html')
-
-@app.route('/about')
-def pagina_sobre():
-    return render_template('sobre.html')
-
-@app.route('/edit/<id>')
+@app.route('/edit/<id>', methods=['GET', 'POST'])
 def edita_item(id):
     tabela = Vendedor.query.get(id)
     tabelas = Vendedor.query.all()
@@ -94,6 +106,20 @@ def edita_item(id):
         db.session.commit() 
         return redirect('/admin')
     return render_template('admin.html', tabelas=tabelas, tabela=tabela) 
+
+
+@app.route('/<id>')
+def idselector(id):
+    tabela = Vendedor.query.get(id)
+    return render_template('admin.html', tabela=tabela)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    tabela = Vendedor.query.get(id)
+    db.session.delete(tabela)
+    db.session.commit()
+    return redirect('/admin')
 
 if __name__ == '__main__':
     db.create_all()
